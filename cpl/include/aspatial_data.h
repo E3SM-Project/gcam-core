cb24c8848bd40546b6d81a0f6fa2aab2f095fa07
Philip Myint <philip.myint@gmail.com>
2026-04-08 05:09:40 -0700

    Started work towards water coupling. Moved specialized version of readRegionalMappingData that can handle both regions and subregions from the DegreeDays class to the parent ASpatialData class and get DegreeDays to use this inherited version from the parent. Will have the water coupling also use this inherited version.

--- cpl/include/aspatial_data.h
+++ cpl/include/aspatial_data.h
@@ -44,8 +44,10 @@
 class ASpatialData {
 public:
     ASpatialData(int aSize);
+    ASpatialData(const int aSize, const bool aReadSubregions);
     ~ASpatialData();
     virtual void readMapping(std::string aFileName);
+    virtual void readRegionalMappingData(const std::string &aFileName);
     virtual double readSpatialData(std::string aFileName, bool aHasLatLon, bool aHasID, bool aCalcTotal);
     virtual double readSpatialData(std::string aFileName, bool aHasLatLon, bool aHasID, bool aCalcTotal, double *aValueArray);
     virtual double readSpatialDataCSV(std::string aFileName, bool aHasLatLon, bool aHasID, bool aCalcTotal);
@@ -59,6 +61,19 @@ public:
     virtual std::vector<int> getIDVector();
     virtual std::vector<double> getLatVector();
     virtual std::vector<double> getLonVector();
+protected:
+    // Boolean indicating whether to read subregions in the mapping file. If false, subregions will be aggregated to their parent regions
+    bool mReadSubregions;
+
+    // Map grid cells to regions. Key is a string with longitude and latitude ("lon_lat").
+    // Key maps to a vector of strings containing the region and subregion. For example, "RegionA.Subregion1". 
+    // Note that this map will contain more than lat * lon entries since some grid cells map to multiple regions
+    std::map<std::string, std::vector<std::string>> mRegionMapping;
+    
+    // Map region weights (these indicate the fraction of a grid cell assigned to each region)
+    // Key is a pair indicating the grid cell and the region/subregion.
+    // Key maps to a double representing the fraction of the grid cell in that region/subregion
+    std::map<std::pair<std::string,std::string>, double> mRegionWeights;
 private:
     std::vector<double> mValueVector;
     std::vector<double> mLatVector;
--- cpl/source/aspatial_data.cpp
+++ cpl/source/aspatial_data.cpp
@@ -35,7 +35,7 @@
  * \brief This file processes gridded data either from file or from memory. It is an abstract
  * class from which the carbon scaler and emissions downscale code is derived.
  *
- * \author Kate Calvin and Alan Di Vittorio
+ * \author Kate Calvin and Alan Di Vittorio and Philip Myint
  */
 
 #include <iostream>
@@ -53,6 +53,14 @@ mLatVector(aSize, 0),
 mLonVector(aSize, 0),
 mIDVector(aSize, 0),
 mValueVector(aSize, 0) {
+}
+
+// Constructor with option to read subregions in the mapping file. If false, subregions will be aggregated to their parent regions.
+// Uses constructor delegation to call the main constructor to initialize the vectors
+ASpatialData::ASpatialData(const int aSize, const bool aReadSubregions):
+ASpatialData(aSize)
+{
+    mReadSubregions = aReadSubregions;
 }
 
 // Destructor
@@ -325,6 +333,159 @@ void ASpatialData::writeSpatialData(std::string aFileName, bool aWriteID) {
     oFile.close();
     
     return;
+}
+
+/*!
+ * \brief Read regional mapping data from CSV file
+ * 
+ * Reads a CSV file that maps E3SM grid cells to GCAM regions. Each grid cell can map to 
+ * one or more GCAM regions (for cells that straddle regional boundaries), with fractional 
+ * weights indicating what portion of the cell belongs to each region.
+ * 
+ * \param aFileName Path to the regional mapping CSV file
+ * 
+ * \details
+ * **CSV File Format:**
+ * The input file must be a comma-separated file with a header row (skipped) and the following columns:
+ * - Column 1: Region ID (skipped)
+ * - Column 2: GLU ID (skipped)
+ * - Column 3: Longitude index (integer)
+ * - Column 4: Latitude index (integer)
+ * - Column 5: Region name (string, may include quotes)
+ * - Column 6: Subregion name (string, may include quotes)
+ * - Column 7: Weight (double, 0.0 to 1.0)
+ * 
+ * **Example CSV:**
+ * \code
+ * region_id,glu_id,lon,lat,region,subregion,weight
+ * 1,1,144,96,"USA","Northwest",1.0
+ * 2,2,144,97,"USA","Northwest",0.6
+ * 2,2,144,97,"Canada","BritishColumbia",0.4
+ * \endcode
+ * 
+ * **Data Structures Populated:**
+ * 
+ * 1. `mRegionMapping`: Maps grid cell ID to list of regions
+ *    - Key: "lon_lat" (e.g., "144_96")
+ *    - Value: Vector of region IDs (e.g., ["USA.Northwest", "Canada.BritishColumbia"])
+ *    - A grid cell may map to multiple regions if it straddles a boundary
+ * 
+ * 2. `mRegionWeights`: Maps (grid cell, region) pair to fractional weight
+ *    - Key: pair<string, string> (gridID, regionID)
+ *    - Value: double (0.0 to 1.0) representing fraction of cell in that region
+ *    - Weights for a single grid cell across all regions should sum to 1.0
+ * 
+ */
+// TODO: Modify all subclasses of ASpatialData (e.g., CarbonScalers, EmissDownscale, DegreeDays) to use this 
+// method to read in the regional mapping data
+void ASpatialData::readRegionalMappingData(const std::string &aFileName) 
+{
+    // Open the file and check that it opened successfully
+    std::ifstream data(aFileName);
+    if (!data.is_open())
+    {
+        exit(EXIT_FAILURE);
+    }
+    std::string line;
+    // Skip the first line (i.e., the header)
+    std::getline(data, line); 
+
+    // Read each line of the file one by one
+    while (std::getline(data, line))
+    {
+        std::istringstream iss(line);
+        std::string token;
+        int lon;
+        int lat;
+        std::string region;
+        std::string subregion;
+        double weight;
+            
+        // Skip region & GLU ID
+        std::getline(iss, token, ',');
+        std::getline(iss, token, ',');
+        
+        // Parse longitude
+        std::getline(iss, token, ',');
+        lon = std::stoi(token);
+        
+        // Parse latitude
+        std::getline(iss, token, ',');
+        lat = std::stoi(token);
+        
+        // Example gridID: "144_96", where 144 is the longitude index and 96 is the latitude index
+        std::string gridID = std::to_string(lon) + "_" + std::to_string(lat);
+        
+        // Parse Region ID
+        std::getline(iss, token, ',');
+        region = token;
+        // Remove quotes from region name, if they exist. TODO: Also remove spaces?
+        region.erase( remove( region.begin(), region.end(), '\"' ), region.end() );
+        
+        std::string regID;
+        if (mReadSubregions)
+        {
+            // Parse Subregion ID
+            std::getline(iss, token, ',');
+            subregion = token;
+            // Remove quotes from subregion name, if they exist. TODO: Also remove spaces?
+            subregion.erase( remove( subregion.begin(), subregion.end(), '\"' ), subregion.end() );
+
+            // Example region ID: "USA.Northwest", where "USA" is the region and "Northwest" is the subregion (e.g., basin, state, or other sub-regional unit)
+            regID = region + "." + subregion;
+
+            // Add region ID to the mapping vector. Note that there maybe more than one regID per gridID (hence, a vector). If gridID is not found, this will 
+            // automatically create a new vector with this regID as the vector's first element. If gridID is found, this will add to the existing regID vector
+            mRegionMapping[gridID].push_back(regID);
+
+            // Parse weight -- this is the fraction of the grid cell in a particular GCAM region
+            std::getline(iss, token, ',');
+            weight = std::stod(token);
+
+            // If reading subregions, then the key is the specific region-subregion combination (e.g., "USA.Northwest"), 
+            // and it will be unique, so we can just add the entry to the map. This will also create the map entry if it does not already exist
+            mRegionWeights[std::make_pair(gridID, regID)] = weight;
+        }
+        else
+        {
+            // If not reading subregions, then skip reading subregions so that the region ID is just the main region (e.g., "USA")
+            std::getline(iss, token, ',');
+            regID = region;
+
+            // Parse weight -- this is the fraction of the grid cell in a particular GCAM region
+            std::getline(iss, token, ',');
+            weight = std::stod(token);
+
+            // Populate the region mapping and weights maps
+            auto it = mRegionMapping.find(gridID);
+            if (it == mRegionMapping.end())
+            {
+                // If gridID is not found, then create a vector with this regID and add it to the map
+                mRegionMapping[gridID] = std::vector<std::string>{regID};
+
+                // Set the region weight for this grid cell and region combination
+                mRegionWeights[std::make_pair(gridID, regID)] = weight;
+            }
+            else
+            {
+                // If gridID is found, then add to the existing regID vector only if it is not already in the vector (to avoid duplicates when 
+                // subregions are being aggregated to the same parent region)
+                auto &regIDs = it->second;
+                if (std::find(regIDs.begin(), regIDs.end(), regID) == regIDs.end())
+                {
+                    regIDs.push_back(regID);
+                }
+
+                // If regID is not already in the map, the [] operator will create a new map entry with a default value of 0, so we can just add the weight
+                // in both the case where the regID is new or already exists in the map
+                mRegionWeights[std::make_pair(gridID, regID)] += weight;
+            }
+
+        } // if (mReadSubregions)
+
+    } // while (std::getline(data, line))
+
+    data.close();
 }
 
 void ASpatialData::readMapping(std::string aFileName) {
